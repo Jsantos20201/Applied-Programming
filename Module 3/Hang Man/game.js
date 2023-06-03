4759cf266968dbedd0631925ed4f3a35d9510d98
Jsantos20201 <72623931+Jsantos20201@users.noreply.github.com>
2023-06-03 10:40:09 -0600

    Working on a few bugs.

---
+++ Module 3/Hang Man/game.js
@@ -0,0 +1,9 @@
+// Import the Hangman class
+import Hangman from './hangman.js';
+
+// List of words for the game
+const words = ['hangman', 'javascript', 'programming', 'openai', 'computer'];
+
+// Create an instance of the Hangman class and start the game
+const hangman = new Hangman(words);
+hangman.play();1
---
+++ Module 3/Hang Man/hangman.js
@@ -0,0 +1,157 @@
+class Hangman {
+    constructor(words) {
+      this.words = words;
+      this.manStages = [
+        // stages omitted for brevity
+        `
+        +---+
+        |   |
+            |
+            |
+            |
+            |
+        =========
+        `,
+        `
+        +---+
+        |   |
+        O   |
+            |
+            |
+            |
+        =========
+        `,
+        `
+        +---+
+        |   |
+        O   |
+        |   |
+            |
+            |
+        =========
+        `,
+        `
+        +---+
+        |   |
+        O   |
+       /|   |
+            |
+            |
+        =========
+        `,
+        `
+        +---+
+        |   |
+        O   |
+       /|\\  |
+            |
+            |
+        =========
+        `,
+        `
+        +---+
+        |   |
+        O   |
+       /|\\  |
+       /    |
+            |
+        =========
+        `,
+        `
+        +---+
+        |   |
+        O   |
+       /|\\  |
+       / \\  |
+            |
+        =========
+      `
+      ];
+      this.selectedWord = '';
+      this.guessedLetters = [];
+      this.maxGuesses = this.manStages.length - 1;
+      this.incorrectGuesses = -1;
+    }
+  
+    getRandomWord() {
+      const randomIndex = Math.floor(Math.random() * this.words.length);
+      this.selectedWord = this.words[randomIndex];
+    }z
+  
+    displayWordState() {
+      let wordState = '';
+      for (let char of this.selectedWord) {
+        if (this.guessedLetters.includes(char)) {
+          wordState += char;
+        } else {
+          wordState += '_';
+        }
+        wordState += ' ';
+      }
+      console.log(wordState);
+    }
+  
+    isGameWon() {
+      for (let char of this.selectedWord) {
+        if (!this.guessedLetters.includes(char)) {
+          return false;
+        }
+      }
+      return true;
+    }
+  
+    processGuess(letter) {
+      if (this.guessedLetters.includes(letter)) {
+        console.log("You already guessed that letter. Try again!");
+      } else {
+        this.guessedLetters.push(letter);
+        if (this.selectedWord.includes(letter)) {
+          console.log("Good guess!");
+          if (this.isGameWon()) {
+            console.log("Congratulations! You won the game!");
+          }
+        } else {
+          console.log("Oops! Wrong guess.");
+          this.incorrectGuesses++;
+          console.log("Remaining guesses: " + (this.maxGuesses - this.incorrectGuesses - 1));
+          if (this.incorrectGuesses >= this.maxGuesses - 1) {
+            console.log("You lost the game. The word was: " + this.selectedWord);
+          } else {
+            console.log(this.manStages[this.incorrectGuesses + 1]);
+          }
+        }
+      }
+    }
+  
+    startGame() {
+      console.log("Welcome to Hangman!");
+      console.log("Guess the word by entering one letter at a time.");
+      console.log("You have " + this.maxGuesses + " guesses. Good luck!");
+      // Display the first picture of manStages
+      console.log(this.manStages[0]);
+      this.getRandomWord();
+      this.displayWordState();
+    }
+  
+    readUserInput() {
+      const guess = prompt("Enter your guess: ");
+      const letter = guess.trim().toLowerCase();
+      this.processGuess(letter);
+      this.displayWordState();
+  
+      // Check if the game is won or lost
+      if (this.isGameWon() || this.incorrectGuesses >= this.maxGuesses) {
+        return;
+      } else {
+        this.readUserInput();
+      }
+    }
+  
+    play() {
+      this.startGame();
+      this.readUserInput();
+    }
+  }
+
+// Export the Hangman class
+export default Hangman; 
